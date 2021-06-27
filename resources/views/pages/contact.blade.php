ae7c9ed2e623ce4aa52e381f708703850f25ba33
Mehdi <elmehdielyassmine@gmail.com>
2021-06-27 22:56:17 +0100

    fix merge

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -30,21 +30,45 @@
       <div class="row block-9 justify-content-center mb-5">
         <div class="col-md-8 mb-md-5">
             <h2 class="text-center">If you got any questions <br>please do not hesitate to send us a message</h2>
-          <form action="#" class="bg-light p-5 contact-form">
+            {{-- @if(Session::has('success'))
+            <div class="alert alert-success">
+                {{ Session::get('success') }}
+                @php
+                    Session::forget('success');
+                @endphp
+            </div>
+            @endif --}}
+
+
+          <form action="{{ route('contact-form.store') }}" class="bg-light p-5 contact-form">
+            {{ csrf_field() }}
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Your Name">
+              <input type="text" class="form-control" placeholder="Your Name" value="{{ old('name') }}" name="name" required>
+              @if ($errors->has('name'))
+                  <span class="text-danger">{{ $errors->first('name') }}</span>
+              @endif
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Your Email">
+              <input type="text" class="form-control" placeholder="Your Email" value="{{ old('email') }}" name="email" required>
+              @if ($errors->has('email'))
+                  <span class="text-danger">{{ $errors->first('email') }}</span>
+              @endif
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Subject">
+              <input type="text" class="form-control" placeholder="Subject" value="{{ old('objet') }}" name="objet" required>
+              @if ($errors->has('objet'))
+                  <span class="text-danger">{{ $errors->first('objet') }}</span>
+              @endif
             </div>
             <div class="form-group">
-              <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+              <textarea name="message" id="" cols="30" rows="7" class="form-control" placeholder="Message">{{ old('message') }}</textarea>
+
+              @if ($errors->has('message'))
+                  <span class="text-danger">{{ $errors->first('message') }}</span>
+              @endif
             </div>
             <div class="form-group">
-              <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+              <input type="submit" value="Send" class="btn btn-primary py-3 px-5">
             </div>
           </form>
           
